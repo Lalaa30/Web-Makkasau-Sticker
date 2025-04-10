6194db3631c99361c6be0ebb8edff03ccd35c7f3
Lalaa30 <badrayantimutiarani302@gmail.com>
2025-04-11 06:20:45 +0700

    Perbaiki design

--- src/components/home/Hero/Hero.jsx
+++ src/components/home/Hero/Hero.jsx
@@ -10,8 +10,8 @@ const Hero = () => {
         <div className="hero-content">
           <div className="hero-subtitle">Selamat Datang di Makkasau Stickers</div>
           <h1>Temukan Plat Kendaraan Custom untuk Setiap Kebutuhan</h1>
-          <p>Makkasau Sticker menyediakan jasa pembuatan custom plat kendaraan premium dengan kualitas terbaik. 
-          Kami memiliki komitmen untuk memberikan layanan terbaik sejak tahun 2015.</p>
+          <p>menyediakan jasa pembuatan custom plat kendaraan premium dengan kualitas terbaik. 
+          Kami memiliki komitmen untuk memberikan layanan terbaik.</p>
           <div className="hero-btns">
           </div>
         </div>
